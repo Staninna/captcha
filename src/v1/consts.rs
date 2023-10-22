3cc5baa1a75fc71c4c5f67580ef258e97a75c3c1
Staninna <26458805+Staninna@users.noreply.github.com>
2023-10-22 23:54:42 +0200

    Made images bigger

--- src/v1/consts.rs
+++ src/v1/consts.rs
@@ -7,6 +7,7 @@ pub const CHARACTERS: &[char] = &[
     'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', //
     'Y', 'Z',
 ];
-pub const WIDTH: u32 = 100;
-pub const HEIGHT: u32 = 40;
+pub const WIDTH: u32 = 100 * 3;
+pub const HEIGHT: u32 = 40 * 3;
 pub const FONT_FILE: &[u8] = include_bytes!("../../fonts/opensans.ttf");
+
