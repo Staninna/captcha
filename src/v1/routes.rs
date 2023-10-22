d5dc6b8e2d9495be5a87eeeccfd8bef04ce5b5f3
Staninna <26458805+Staninna@users.noreply.github.com>
2023-10-22 18:49:32 +0200

    Made parameters full on routes

--- src/v1/routes.rs
+++ src/v1/routes.rs
@@ -3,9 +3,9 @@ use rocket::{delete, fs::NamedFile, get, response::status::NotFound, serde::json
 use serde::Serialize;
 use tempfile::TempDir;
 
-#[get("/captcha/new?<len>")]
-pub async fn new_captcha(len: usize, state: &State<TempDir>) -> Json<Captcha> {
-    let captcha = Captcha::new(len, state);
+#[get("/captcha/new?<length>")]
+pub async fn new_captcha(length: usize, state: &State<TempDir>) -> Json<Captcha> {
+    let captcha = Captcha::new(length, state);
     Json(captcha)
 }
 
@@ -15,12 +15,12 @@ pub struct CaptchaResponse {
     id: String,
 }
 
-#[get("/captcha/image?<id>")]
+#[get("/captcha/image?<captcha_id>")]
 pub async fn get_captcha_img(
-    id: String,
+    captcha_id: String,
     state: &State<TempDir>,
 ) -> Result<NamedFile, NotFound<Json<ErrorMsg>>> {
-    let file_path = state.path().join(id.clone() + ".png");
+    let file_path = state.path().join(captcha_id.clone() + ".png");
     dbg!(&file_path);
 
     let file = NamedFile::open(&file_path).await;
@@ -29,20 +29,22 @@ pub async fn get_captcha_img(
         Ok(file) => Ok(file),
         Err(_) => Err(NotFound(Json(ErrorMsg::new(
             "Captcha not found".to_string(),
-            Some(id),
+            Some(captcha_id),
         )))),
     }
 }
 
 // Delete the captcha image from the server
-#[delete("/captcha/delete?<id>")]
-pub async fn delete_captcha(id: String, state: &State<TempDir>) -> Json<ErrorMsg> {
-    let file_path = state.path().join(id.clone() + ".png");
-
+#[delete("/captcha/delete?<captcha_id>")]
+pub async fn delete_captcha(captcha_id: String, state: &State<TempDir>) -> Json<ErrorMsg> {
+    let file_path = state.path().join(captcha_id.clone() + ".png");
     let result = std::fs::remove_file(file_path);
 
     match result {
         Ok(_) => Json(ErrorMsg::new("Captcha deleted".to_string(), None)),
-        Err(_) => Json(ErrorMsg::new("Captcha not found".to_string(), Some(id))),
+        Err(_) => Json(ErrorMsg::new(
+            "Captcha not found".to_string(),
+            Some(captcha_id),
+        )),
     }
 }
