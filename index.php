78cf4fbac785944a38f536bf8f9e93c8fe9fc1aa
Staninna <26458805+Staninna@users.noreply.github.com>
2023-10-28 14:38:10 +0200

    Updated example app

--- index.php
+++ index.php
@@ -17,58 +17,102 @@ function postRequest($url) {
     return $response;
 }
 
-function createNewCaptcha($authToken, $level = 2) {
-    $url = "/api/v1/new?auth=$authToken&level=$level";
-    $response = getReqest($url);
-    return json_decode($response, true);
-}
-
-function getCaptchaImageURL($captchaId, $authToken) {
-    $url = "/api/v1/image_url?id=$captchaId&auth=$authToken";
+function createNewCaptcha(
+    $length,
+    $width,
+    $height,
+    $filters,
+    $authToken = AUTH_TOKEN,
+) {
+    $url = "/api/v1/new?auth=$authToken&length=$length&width=$width&height=$height&filters=$filters";
     $response = getReqest($url);
     return $response;
 }
 
-function verifyCaptchaCode($captchaId, $code, $authToken) {
+function verifyCaptchaCode($captchaId, $code, $authToken = AUTH_TOKEN) {
     $url = "/api/v1/verify?id=$captchaId&code=$code&auth=$authToken";
     $response = postRequest($url);
     return $response;
 }
 
-const BASE_URL = "http://127.0.0.1:8000";
-const AUTH_TOKEN = "TOKEN";
+function head() {
+    echo '<head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Captcha Example</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    </head>';
+}
+
+// Authentication token
+const AUTH_TOKEN = "secret";
+
+// Base URL of the API
+const BASE_URL = "http://localhost:8000";
+
+// Captcha settings
+const CAPTCHA_LENGTH = 6;
+const CAPTCHA_WIDTH = 200;
+const CAPTCHA_HEIGHT = 100;
+const CAPTCHA_FILTERS = null;
 
 session_start();
-if (!isset($_SESSION['level'])) {
-    $_SESSION['level'] = 2;
-}
-
-if (isset($_GET['level'])) {
-    $_SESSION['level'] = $_GET['level'];
-}
-
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-    $captchaId = createNewCaptcha(AUTH_TOKEN, $_SESSION['level'])['id'];
-    $captchaImageUrl = getCaptchaImageURL($captchaId, AUTH_TOKEN);
-    $response = json_decode($captchaImageUrl, true);
-    $captchaImageUrl = $response['url'];
-} else if (
-    $_SERVER['REQUEST_METHOD'] === 'POST' &&
-    isset($_POST['code']) && isset($_POST['captchaId'])
-) {
-    $code = $_POST['code'];
-    $captchaId = $_POST['captchaId'];
-    $response = verifyCaptchaCode($captchaId, $code, AUTH_TOKEN);
-    $response = json_decode($response, true);
+    // Update session variables
+    if (isset($_GET['length']))  { $_SESSION['length']  = $_GET['length'];  }
+    if (isset($_GET['width']))   { $_SESSION['width']   = $_GET['width'];   }
+    if (isset($_GET['height']))  { $_SESSION['height']  = $_GET['height'];  }
+    if (isset($_GET['filters'])) { $_SESSION['filters'] = $_GET['filters']; }
 
-    echo '<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">';
-    if ($response['ok'] == 'Captcha verified') {
-        echo '<h1>Captcha verified</h1>';
-    } else {
-        echo '<h1>Captcha not verified</h1>';
+
+    // Generate new captcha
+    $captcha = createNewCaptcha(
+        length: $_SESSION['length'] ?? CAPTCHA_LENGTH,
+        width: $_SESSION['width'] ?? CAPTCHA_WIDTH,
+        height: $_SESSION['height'] ?? CAPTCHA_HEIGHT,
+        filters: $_SESSION['filters'] ?? CAPTCHA_FILTERS,
+    );
+
+    // Decode captcha response
+    $captcha = json_decode($captcha, true);
+
+    // Check if captcha is generated successfully
+    if (!isset($captcha['id']) && !isset($captcha['url']) && !isset($captcha['expire_time'])) {
+        die("An error occured while generating captcha");
     }
 
-    echo '<a href="index.php">Go back</a>';
+    // Get captcha details
+    $captchaId = $captcha['id'];
+    $captchaUrl = $captcha['url'];
+    $captchaExpiresAt = $captcha['expire_time'];
+}
+
+// Verify captcha
+else if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $captchaId = $_POST['captchaId'];
+    $code = $_POST['code'];
+    $verifyCaptcha = verifyCaptchaCode($captchaId, $code);
+
+    // Decode captcha response
+    $response = json_decode($verifyCaptcha, true);
+
+    echo "<html lang='en'>";
+    head();
+    echo "<div class='container'>";
+    if (isset($response['ok'])) {
+        echo "<h1 style='color: green;'>Response is OK</h1>";
+        echo "<div class='alert alert-success'>" . $response['ok'] . "</div>";
+    } else if (isset($response['warn'])) {
+        echo "<h1 style='color: orange;'>Response is Warning</h1>";
+        echo "<div class='alert alert-warning'>" . $response['warn'] . "</div>";
+    } else if (isset($response['error'])) {
+        echo "<h1 style='color: red;'>Response is Error</h1>";
+        echo "<div class='alert alert-danger'>" . $response['error'] . "</div>";
+    } else {
+        echo "<h1 style='color: red;'>Unknown Response</h1>";
+        echo "<div class='alert alert-danger'>" . $response . "</div>";
+    }
+    echo "<a href='index.php'>Go Back</a>";
     exit();
 }
 
@@ -86,42 +130,50 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
 
 <body>
     <div class="container">
+        <h1>Capthca Example</h1>
         <div class="col-md-6">
-            <h2>Capthca Image</h2>
-            <img src="<?= $captchaImageUrl ?>" alt="CAPTCHA">
+            <h2>Captcha Image</h2>
+            <img src="<?= $captchaUrl ?>" alt="Captcha Image">
+            <hr>
+            <h2>Verify Captcha</h2>
+            <form method="POST" action="index.php">
+                <div class="form-group">
+                    <input type="hidden" name="captchaId" value="<?= $captchaId ?>">
+                    <label for="code">Enter the CAPTCHA Code:</label>
+                    <input type="text" name="code" id="code" class="form-control" required>
+                </div>
+                <button type="submit" class="btn btn-primary">Verify CAPTCHA</button>
+            </form>
+            <hr>
+            <h3>Settings</h3>
             <form method="GET" action="index.php">
                 <div class="form-group">
-                    <label for="level">Select CAPTCHA Level:</label>
-                    <select name="level" id="level" class="form-control">
-                        <?php
-                        // Generate options for levels 1 to 9
-                        for ($i = 1; $i <= 9; $i++) {
-                            if ($_SESSION['level'] == $i) {
-                                echo "<option value='$i' selected>$i</option>";
-                            } else {
-                                echo "<option value='$i'>$i</option>";
-                            }
-                        }
-                        ?>
-                    </select>
+                    <label for="length">Length:</label>
+                    <input type="number" name="length" id="length" class="form-control" value="<?= $_SESSION['length'] ?? CAPTCHA_LENGTH ?>">
                 </div>
-                <button type="submit" class="btn btn-primary">Set CAPTCHA Level</button>
-                <a href="index.php">
-                    <button type="button" class="btn btn-primary">Generate New CAPTCHA</button>
-                </a>
-            </form>
+                <div class="form-group">
+                    <label for="width">Width:</label>
+                    <input type="number" name="width" id="width" class="form-control" value="<?= $_SESSION['width'] ?? CAPTCHA_WIDTH ?>">
+                </div>
+                <div class="form-group">
+                    <label for="height">Height:</label>
+                    <input type="number" name="height" id="height" class="form-control" value="<?= $_SESSION['height'] ?? CAPTCHA_HEIGHT ?>">
+                </div>
+                <div class="form-group">
+                    <label for="filters">Filters:</label>
+                    <input type="text" name="filters" id="filters" class="form-control" value="<?= $_SESSION['filters'] ?? CAPTCHA_FILTERS ?>">
+                    <small class="form-text text-muted">For more info about the filters syntax goto <a href="<?= BASE_URL ?>/api/v1/help/filters" target="_blank"><?= BASE_URL ?>/api/v1/help/filters</a></small>
+                </div>
 
-            <form method="POST" action="index.php">
-                <div class="col-md-6">
-                    <h2>Verify Captcha</h2>
-                    <div class="form-group">
-                        <input type="hidden" name="captchaId" value="<?= $captchaId ?>">
-                        <label for="code">Enter the CAPTCHA Code:</label>
-                        <input type="text" name="code" id="code" class="form-control" required>
-                    </div>
-                    <button type="submit" class="btn btn-primary">Verify CAPTCHA</button>
-                </div>
+                <button type="submit" class="btn btn-primary">Generate New Captcha</button>
             </form>
+            <hr>
+            <h3>Details</h3>
+            <ul>
+                <li>Captcha ID: <code><?= $captchaId ?></code></li>
+                <li>Captcha Expires At: <code><?= $captchaExpiresAt ?></code></li>
+                <li>Captcha URL: <code><?= $captchaUrl ?></code></li>
+            </ul>
         </div>
     </div>
 </body>
