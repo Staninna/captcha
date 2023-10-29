32f4ba26bdb44cccef4dbbce67865664d210cb49
Staninna <26458805+Staninna@users.noreply.github.com>
2023-10-29 13:15:53 +0100

    Added todo for when i am bored

--- src/v1/mod.rs
+++ src/v1/mod.rs
@@ -7,6 +7,7 @@ pub use routes::{captcha_image_url_redirect, filter_docs, help, new_captcha, ver
 mod response;
 pub use response::Response;
 
+// TODO: Check if rocket can manage auth_token, captchas, urls and config without AppState
 mod app_state;
 pub use app_state::{AppState, AppStatePointer, ConfigKey, ConfigValue};
 
